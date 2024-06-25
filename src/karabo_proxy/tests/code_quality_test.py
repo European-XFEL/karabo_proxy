8083dd59228cf9a6631a59d1c4181e0d5fd2a87a
Dennis Goeries <dennis.goeries@xfel.eu>
2024-06-25 16:47:12 +0200

    Add async channel client

---
+++ src/karabo_proxy/tests/code_quality_test.py
@@ -0,0 +1,27 @@
+import os
+import os.path as op
+import subprocess
+
+import karabo_proxy
+
+IGNORE_LIST = ['setup.py', '__init__.py']
+
+
+def get_python_files():
+    """Get all python files from this package
+    """
+    common_dir = op.abspath(op.dirname(karabo_proxy.__file__))
+    flake_check = []
+    for dirpath, _, filenames in os.walk(common_dir):
+        for fn in filenames:
+            if op.splitext(fn)[-1].lower() == '.py' and fn not in IGNORE_LIST:
+                path = op.join(dirpath, fn)
+                flake_check.append(path)
+
+    return flake_check
+
+
+def test_code_quality_flake8():
+    files = get_python_files()
+    command = ['flake8', *[op.abspath(py_file) for py_file in files]]
+    subprocess.check_call(command)
