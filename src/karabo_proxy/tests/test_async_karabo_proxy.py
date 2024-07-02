3e6a1beaaf71924c4fa0be849a27267ec145b48b
Raul Costa <raul.costa@xfel.eu>
2024-07-02 14:02:57 +0200

    Synchronous get topology operation

--- src/karabo_proxy/tests/test_async_karabo_proxy.py
+++ src/karabo_proxy/tests/test_async_karabo_proxy.py
@@ -1,49 +1,79 @@
-import asyncio
+import os
+import subprocess
+from time import sleep
 
 import pytest
-import pytest_asyncio
 
 from ..async_karabo_proxy import AsyncKaraboProxy
 from ..schemas.web_proxy_responses import TopologyInfo
-from .mock_web_proxy import (
-    PORT_INVALID_MOCK, PORT_VALID_MOCK, run_invalid_mock, run_valid_mock)
+from ..sync_karabo_proxy import SyncKaraboProxy
+from .mock_web_proxy import PORT_INVALID_MOCK, PORT_VALID_MOCK
 
 
-@pytest.fixture(scope="session")
-def event_loop():
-    """Overrides pytest default function scoped event loop"""
-    loop = asyncio.get_event_loop()
-    yield loop
-    # Note: the aiohttp server does not close gracefully, allowing a warning
-    # about pending tasks being destroyed at the end of the test. The problem
-    # is a known issue of aiohttp and is expected to be solved on an upcoming
-    # version 4.0: https://github.com/aio-libs/aiohttp/issues/4408.
-    loop.close()
+@pytest.fixture(scope="module")
+def web_proxy_mocks():
+    mock_module_path = os.path.dirname(
+        os.path.abspath(os.path.dirname(__file__)))
+    proc_valid_mock = subprocess.Popen(
+        ["python", "-m", "tests.mock_web_proxy"],
+        cwd=mock_module_path)
+    proc_invalid_mock = subprocess.Popen(
+        ["python", "-m", "tests.mock_web_proxy", "--invalid"],
+        cwd=mock_module_path)
+    # The WebProxy mocks require some time to initialize and reach
+    # the point when they can start accepting requests.
+    sleep(2)
+    yield
+    proc_valid_mock.terminate()
+    proc_invalid_mock.terminate()
 
 
-@pytest_asyncio.fixture(scope="module")
-async def valid_mock_cli():
-    """Instantiantes a client for the WebProxy mock that returns valid
-    responses (after launching the mock, a web server application)"""
-    await run_valid_mock()
+@pytest.fixture(scope="module")
+def valid_mock_async_cli():
+    """Instantiantes an async client for the WebProxy mock that returns valid
+    responses"""
     return AsyncKaraboProxy(f"http://localhost:{PORT_VALID_MOCK}")
 
 
-@pytest_asyncio.fixture(scope="module")
-async def invalid_mock_cli():
-    """Instantiantes a client for the WebProxy mock that returns invalid
-    responses (after launching the mock, a web server application)"""
-    await run_invalid_mock()
+@pytest.fixture(scope="module")
+def invalid_mock_async_cli():
+    """Instantiantes an async client for the WebProxy mock that returns
+    invalid responses"""
     return AsyncKaraboProxy(f"http://localhost:{PORT_INVALID_MOCK}")
 
 
-@pytest.mark.timeout(30)
+@pytest.fixture(scope="module")
+def valid_mock_sync_cli():
+    """Instantiantes a sync client for the WebProxy mock that returns valid
+    responses"""
+    return SyncKaraboProxy(f"http://localhost:{PORT_VALID_MOCK}")
+
+
+@pytest.fixture(scope="module")
+def invalid_mock_sync_cli():
+    """Instantiantes a sync client for the WebProxy mock that returns
+    invalid responses"""
+    return SyncKaraboProxy(f"http://localhost:{PORT_INVALID_MOCK}")
+
+
 @pytest.mark.asyncio
-async def test_get_topology(event_loop, valid_mock_cli, invalid_mock_cli):
-    # Checks that a correct request to the valid mock succeeds
-    topology = await valid_mock_cli.get_topology()
+async def test_get_topology(web_proxy_mocks,
+                            valid_mock_async_cli,
+                            valid_mock_sync_cli,
+                            invalid_mock_async_cli,
+                            invalid_mock_sync_cli):
+    # Checks that a correct async request to the valid mock succeeds
+    topology = await valid_mock_async_cli.get_topology()
     assert (type(topology) is TopologyInfo)
-    # Checks that a correct request to the invalid mock fails due to the
+    # Checks that a correct async request to the invalid mock fails due to the
     # invalid response
     with pytest.raises(RuntimeError, match="Invalid response format"):
-        topology = await invalid_mock_cli.get_topology()
+        topology = await invalid_mock_async_cli.get_topology()
+
+    # Checks that a correct sync request to the valid mock succeeds
+    topology = valid_mock_sync_cli.get_topology()
+    assert (type(topology) is TopologyInfo)
+    # Checks that a correct sync request to the invalid mock fails due to the
+    # invalid response
+    with pytest.raises(RuntimeError, match="Invalid response format"):
+        topology = invalid_mock_sync_cli.get_topology()
